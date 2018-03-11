7b35cd5ff7c9f6fc7691a8634921469fab25f46b
Ian M <ianmorrill@icloud.com>
2018-03-11 17:45:10 -0400

    um

--- src/src.h
+++ src/src.h
@@ -3,9 +3,12 @@
 
 //// DOTSTAR CONFIG ////
 
-#define NUMPIXELS 100
-#define DATAPIN 35
-#define CLOCKPIN 32
+#define NUMPIXELS1 121
+#define NUMPIXELS2 46
+#define DATAPIN1 35
+#define CLOCKPIN1 32
+#define DATAPIN2 30
+#define CLOCKPIN2 33
 
 // INTERNAL INCLUDES //
 #include <Adafruit_DotStar.h>
@@ -13,7 +16,7 @@
 #include "Pattern.h"
 
 // CONFIGURATION DEFINES //
-#define numarrays 5
+#define numarrays 4
 
 
 #endif
--- src/src.ino
+++ src/src.ino
@@ -1,6 +1,6 @@
 #include "src.h"
 
-#define TEST_MODE_ENABLED
+//#define TEST_MODE_ENABLED
 
 #ifdef TEST_MODE_ENABLED
 
@@ -17,237 +17,131 @@
 
 #endif
 
+/*
+ * 44 - Length of bumper lights
+ * 77 - Length of top light
+ * 38 - Top split point
+ */
+
 Pattern patterns[numarrays];
 
-Adafruit_DotStar strip = Adafruit_DotStar(NUMPIXELS, DATAPIN, CLOCKPIN, DOTSTAR_BGR);
-
-//int scrollIncr = 0;
+Adafruit_DotStar strip1 = Adafruit_DotStar(NUMPIXELS1, DATAPIN1, CLOCKPIN1, DOTSTAR_BGR);
+Adafruit_DotStar strip2 = Adafruit_DotStar(NUMPIXELS2, DATAPIN2, CLOCKPIN2, DOTSTAR_BGR);
 
 void setup() {
-  strip.begin(); 
-  strip.show();
+  strip1.begin(); 
+  strip1.show();
+  strip2.begin(); 
+  strip2.show();
 
   COM.SETUP;
 
-  for (int i = 0; i < 5; i++) {
-    patterns[i] = Pattern();
-  }
-//  patterns[0].setStrip(&strip);
-//  patterns[0].setLocation(0, 30);
-//  patterns[0].aze(4, true);
-//
-//  patterns[1].setStrip(&strip);
-//  patterns[1].setLocation(30, 60);
-//  patterns[1].scroll(0xff0000, 0x00ff00, 6, false);
-//
-//  patterns[2].setStrip(&strip);
-//  patterns[2].setLocation(60, 90);
-//  patterns[2].scroll(0xffffff, 0x000000, 10, true);
-//  
-//  patterns[3].setStrip(&strip);
-//  patterns[3].setLocation(90, 99);
-//  patterns[3].flash(0xff0000, 0x000000, 500);
+  patterns[0].setStrip(&strip1);
+  patterns[1].setStrip(&strip1);
+  patterns[2].setStrip(&strip1);
+  patterns[3].setStrip(&strip2);
 }
 
 void loop() {
   for (int i = 0; i < numarrays; i++) {
     patterns[i].setPixels();
+    
   }
-  strip.show();
+  strip1.show();
+  strip2.show();
   if (COM.available() > 0) {
     int code = COM_READ;
     switch (code) {
+      case 8:
+      patterns[0].setLocation(0, 44);
+      patterns[0].scroll(0xff0000, 0x000000, 44, false);
+      patterns[1].setLocation(44, 82);
+      patterns[1].scroll(0xff0000, 0x000000, 4, false);
+      patterns[2].setLocation(82, NUMPIXELS1);
+      patterns[2].scroll(0xff0000, 0x000000, 4, true);
+      patterns[3].setLocation(0, 44);
+      patterns[3].scroll(0xff0000, 0x000000, 44, false);
+      break;
+      case 7:
+      patterns[0].setLocation(0, 44);
+      patterns[0].scroll(0xff00ff, 0xff7f00, 4, true);
+      patterns[1].setLocation(44, 82);
+      patterns[1].flash(0xff00ff, 0xff7f00, 100);
+      patterns[2].setLocation(82, NUMPIXELS1);
+      patterns[2].flash(0xff00ff, 0xff7f00, 100);
+      patterns[3].setLocation(0, 44);
+      patterns[3].scroll(0xff00ff, 0xff7f00, 4, true);
+      break;
+      case 6:
+      patterns[0].setLocation(0, 44);
+      patterns[0].scroll(0xffff00, 0xff0000, 4, false);
+      patterns[1].setLocation(44, 82);
+      patterns[1].flash(0xffff00, 0xff0000, 100);
+      patterns[2].setLocation(82, NUMPIXELS1);
+      patterns[2].flash(0xffff00, 0xff0000, 100);
+      patterns[3].setLocation(0, 44);
+      patterns[3].scroll(0xffff00, 0xff0000, 4, false);
+      break;
+      case 5:
+      patterns[0].setLocation(0, 44);
+      patterns[0].scroll(0xffff00, 0xff7f00, 4, false);
+      patterns[1].setLocation(44, 82);
+      patterns[1].flash(0xffff00, 0xff7f00, 100);
+      patterns[2].setLocation(82, NUMPIXELS1);
+      patterns[2].flash(0xffff00, 0xff7f00, 100);
+      patterns[3].setLocation(0, 44);
+      patterns[3].scroll(0xffff00, 0xff7f00, 4, false);
+      break;
+      case 4:
+      patterns[0].setLocation(0, 44);
+      patterns[0].scroll(0xffff00, 0x00ff00, 4, false);
+      patterns[1].setLocation(44, 82);
+      patterns[1].flash(0xffff00, 0x00ff00, 100);
+      patterns[2].setLocation(82, NUMPIXELS1);
+      patterns[2].flash(0xffff00, 0x00ff00, 100);
+      patterns[3].setLocation(0, 44);
+      patterns[3].scroll(0xffff00, 0x00ff00, 4, false);
+      break;
       case 3:
-      patterns[0].setStrip(&strip);
-      patterns[0].setLocation(0, 100);
-      patterns[0].flash(0xffff00, 0x000000, 500);
-      patterns[1].setStrip(&strip);
-      patterns[1].setLocation(0, 0);
-      patterns[1].goBlack();
-      patterns[2].setStrip(&strip);
-      patterns[2].setLocation(0, 0);
-      patterns[2].goBlack();
-      patterns[3].setStrip(&strip);
-      patterns[3].setLocation(0, 0);
-      patterns[3].goBlack();
-      patterns[4].setStrip(&strip);
-      patterns[4].setLocation(0, 0);
-      patterns[4].goBlack();
+      patterns[0].setLocation(0, 44);
+      patterns[0].scroll(0xffff00, 0x000000, 4, false);
+      patterns[1].setLocation(44, 82);
+      patterns[1].flash(0xffff00, 0x000000, 100);
+      patterns[2].setLocation(82, NUMPIXELS1);
+      patterns[2].flash(0xffff00, 0x000000, 100);
+      patterns[3].setLocation(0, 44);
+      patterns[3].scroll(0xffff00, 0x000000, 4, false);
       break;
       case 2:
-      patterns[0].setStrip(&strip);
-      patterns[0].setLocation(0, 100);
-      patterns[0].scroll(0xff8e00, 0x060300, 8, false);
-      patterns[1].setStrip(&strip);
-      patterns[1].setLocation(0, 0);
-      patterns[1].goBlack();
-      patterns[2].setStrip(&strip);
-      patterns[2].setLocation(0, 0);
-      patterns[2].goBlack();
-      patterns[3].setStrip(&strip);
-      patterns[3].setLocation(0, 0);
-      patterns[3].goBlack();
-      patterns[4].setStrip(&strip);
-      patterns[4].setLocation(0, 0);
-      patterns[4].goBlack();
+      patterns[0].setLocation(0, 44);
+      patterns[0].scroll(0x0000ff, 0x000000, 44, false);
+      patterns[1].setLocation(44, 82);
+      patterns[1].scroll(0x0000ff, 0x000000, 4, false);
+      patterns[2].setLocation(82, NUMPIXELS1);
+      patterns[2].scroll(0x0000ff, 0x000000, 4, true);
+      patterns[3].setLocation(0, 44);
+      patterns[3].scroll(0x0000ff, 0x000000, 44, false);
       break;
       case 1:
-      patterns[0].setStrip(&strip);
-      patterns[0].setLocation(0, 100);
-      patterns[0].aze(4, false);
-      patterns[1].setStrip(&strip);
-      patterns[1].setLocation(0, 0);
-      patterns[1].goBlack();
-      patterns[2].setStrip(&strip);
-      patterns[2].setLocation(0, 0);
-      patterns[2].goBlack();
-      patterns[3].setStrip(&strip);
-      patterns[3].setLocation(0, 0);
-      patterns[3].goBlack();
-      patterns[4].setStrip(&strip);
-      patterns[4].setLocation(0, 0);
-      patterns[4].goBlack();
+      patterns[0].setLocation(0, 44);
+      patterns[0].aze(6, false);
+      patterns[1].setLocation(44, 82);
+      patterns[1].aze(2, true);
+      patterns[2].setLocation(82, NUMPIXELS1);
+      patterns[2].aze(2, false);
+      patterns[3].setLocation(0, 44);
+      patterns[3].aze(6, false);
       break;
       default:
-      patterns[0].setStrip(&strip);
-      patterns[0].setLocation(0, 100);
+      patterns[0].setLocation(0, NUMPIXELS1);
       patterns[0].goBlack();
-      patterns[1].setStrip(&strip);
       patterns[1].setLocation(0, 0);
       patterns[1].goBlack();
-      patterns[2].setStrip(&strip);
       patterns[2].setLocation(0, 0);
       patterns[2].goBlack();
-      patterns[3].setStrip(&strip);
-      patterns[3].setLocation(0, 0);
+      patterns[3].setLocation(0, NUMPIXELS2);
       patterns[3].goBlack();
-      patterns[4].setStrip(&strip);
-      patterns[4].setLocation(0, 0);
-      patterns[4].goBlack();
       break;
     }
   }
 }
-
-//void aze() {
-//  twoColorScroll(blue, yellow, 10, 50, false);
-//}
-//
-//void solid(uint32_t color) {
-//  solidAlternate(color, color, 1);
-//}
-//
-//void policeFlash(uint32_t color1, uint32_t color2, int len) {
-//  switch ((millis() / 100L) % 6) {
-//    case 0:
-//    solidAlternate(color1, 0, len);
-//    break;
-//    case 2:
-//    solidAlternate(color1, 0, len);
-//    break;
-//    case 3:
-//    solidAlternate(0, color2, len);
-//    break;
-//    case 5:
-//    solidAlternate(0, color2, len);
-//    break;
-//    default:
-//    solidAlternate(0, 0, 1);
-//    break;
-//  }
-//}
-//
-//void solidAlternate(uint32_t color1, uint32_t color2, int len) {
-//  for (int led = 0; led < NUMPIXELS; led++) {
-//    int offsetLED = led % (len * 2);
-//    if (offsetLED >= len) {
-//      strip.setPixelColor(led, color1);
-//    }else{
-//      strip.setPixelColor(led, color2);
-//    }
-//  }
-//  strip.show();
-//}
-//
-//void alternateColors(uint32_t stage1Color1, uint32_t stage1Color2, uint32_t stage1Color3, uint32_t stage2Color1, uint32_t stage2Color2, uint32_t stage2Color3, int len, int scrollTime, bool comeIn, unsigned long altInterval) {
-//  if ((millis() / altInterval) % 2 == 0) {
-//    colorScroll(stage1Color1, stage1Color2, stage1Color3, len, scrollTime, comeIn);
-//  }else{
-//    colorScroll(stage2Color1, stage2Color2, stage2Color3, len, scrollTime, comeIn);
-//  }
-//}
-//
-//void alternate2Colors(uint32_t stage1Color1, uint32_t stage1Color2, uint32_t stage2Color1, uint32_t stage2Color2, int len, int scrollTime, bool comeIn, unsigned long altInterval) {
-//  if ((millis() / altInterval) % 2 == 0) {
-//    twoColorScroll(stage1Color1, stage1Color2, len, scrollTime, comeIn);
-//  }else{
-//    twoColorScroll(stage2Color1, stage2Color2, len, scrollTime, comeIn);
-//  }
-//}
-//
-//void twoColorScroll(uint32_t firstColor, uint32_t secondColor, int len, int scrollTime, bool comeIn, int startLED, int endLED) {
-//  if (scrollIncr >= (len * 2)) {
-//    scrollIncr = 0;
-//  }
-//  if (scrollIncr <= 0) {
-//    scrollIncr = (len * 2) - 1;
-//  }
-//  for (int led = startLED; led < endLED; led++) {
-//    int offsetLED = (led + scrollIncr) % (len * 2);
-//    if (offsetLED > 0 && offsetLED < (len + 1)) {
-//      strip.setPixelColor(led, firstColor);
-//    }else{
-//      strip.setPixelColor(led, secondColor);
-//    }
-//  }
-//  strip.show();
-//  delay(scrollTime);
-//  scrollIncr += comeIn ? 1 : -1;
-//}
-//
-//void fourColorScroll(uint32_t firstColor, uint32_t secondColor, uint32_t thirdColor, uint32_t fourthColor, int len, int scrollTime, bool comeIn, int startLED, int endLED) {
-//  if (scrollIncr >= (len * 4)) {
-//    scrollIncr = 0;
-//  }
-//  if (scrollIncr <= 0) {
-//    scrollIncr = (len * 4) - 1;
-//  }
-//  for (int led = startLED; led < endLED; led++) {
-//    int offsetLED = (led + scrollIncr) % (len * 4);
-//    if (offsetLED > 0 && offsetLED < (len + 1)) {
-//      strip.setPixelColor(led, firstColor);
-//    }else if (offsetLED > len && offsetLED < (len * 2 + 1)) {
-//      strip.setPixelColor(led, secondColor);
-//    }else if (offsetLED > (len * 2) && offsetLED < (len * 3 + 1)) {
-//      strip.setPixelColor(led, thirdColor);
-//    }else{
-//      strip.setPixelColor(led, fourthColor);
-//    }
-//  }
-//  strip.show();
-//  delay(scrollTime);
-//  scrollIncr += comeIn ? 1 : -1;
-//}
-//
-//void colorScroll(uint32_t firstColor, uint32_t secondColor, uint32_t thirdColor, int len, int scrollTime, bool comeIn, int startLED, int endLED) {
-//  if (scrollIncr >= (len * 3)) {
-//    scrollIncr = 0;
-//  }
-//  if (scrollIncr <= 0) {
-//    scrollIncr = (len * 3) - 1;
-//  }
-//  for (int led = startLED; led < endLED; led++) {
-//    int offsetLED = (led + scrollIncr) % (len * 3);
-//    if (offsetLED > 0 && offsetLED < (len + 1)) {
-//      strip.setPixelColor(led, firstColor);
-//    }else if (offsetLED > len && offsetLED < (len * 2 + 1)) {
-//      strip.setPixelColor(led, secondColor);
-//    }else{
-//      strip.setPixelColor(led, thirdColor);
-//    }
-//  }
-//  strip.show();
-//  delay(scrollTime);
-//  scrollIncr += comeIn ? 1 : -1;
-//}
-
