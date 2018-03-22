91a8446a15c6bf070e0e0cb18b45732a7cf2156e
Ian M <ianmorrill@icloud.com>
2018-03-22 18:51:03 -0400

    Before something

--- src/src.h
+++ src/src.h
@@ -1,14 +1,33 @@
 #ifndef SRC_H
 #define SRC_H
 
-//// DOTSTAR CONFIG ////
+//// DOTSTAR CONFIGS ////
 
-#define NUMPIXELS1 121
-#define NUMPIXELS2 46
-#define DATAPIN1 35
-#define CLOCKPIN1 32
-#define DATAPIN2 30
-#define CLOCKPIN2 33
+#define NUMPIXELS 48
+
+#define DATAPIN1 32
+#define CLOCKPIN1 35
+
+#define DATAPIN2 33
+#define CLOCKPIN2 30
+
+#define DATAPIN3 37
+#define CLOCKPIN3 34
+
+#define DATAPIN4 36
+#define CLOCKPIN4 39
+
+#define DATAPIN5 25
+#define CLOCKPIN5 24
+
+#define DATAPIN6 26
+#define CLOCKPIN6 27
+
+#define DATAPIN7 40
+#define CLOCKPIN7 41
+
+#define DATAPIN8 43
+#define CLOCKPIN8 42
 
 // INTERNAL INCLUDES //
 #include <Adafruit_DotStar.h>
@@ -16,7 +35,7 @@
 #include "Pattern.h"
 
 // CONFIGURATION DEFINES //
-#define numarrays 4
+#define numarrays 8
 
 
 #endif
--- src/src.ino
+++ src/src.ino
@@ -1,3 +1,5 @@
+//  LIGHTS ARE 48 LONG
+
 #include "src.h"
 
 //#define TEST_MODE_ENABLED
@@ -17,131 +19,184 @@
 
 #endif
 
-/*
- * 44 - Length of bumper lights
- * 77 - Length of top light
- * 38 - Top split point
- */
-
 Pattern patterns[numarrays];
 
-Adafruit_DotStar strip1 = Adafruit_DotStar(NUMPIXELS1, DATAPIN1, CLOCKPIN1, DOTSTAR_BGR);
-Adafruit_DotStar strip2 = Adafruit_DotStar(NUMPIXELS2, DATAPIN2, CLOCKPIN2, DOTSTAR_BGR);
+Adafruit_DotStar strip1 = Adafruit_DotStar(NUMPIXELS, DATAPIN1, CLOCKPIN1, DOTSTAR_BGR);
+Adafruit_DotStar strip2 = Adafruit_DotStar(NUMPIXELS, DATAPIN2, CLOCKPIN2, DOTSTAR_BGR);
+Adafruit_DotStar strip3 = Adafruit_DotStar(NUMPIXELS, DATAPIN3, CLOCKPIN3, DOTSTAR_BGR);
+Adafruit_DotStar strip4 = Adafruit_DotStar(NUMPIXELS, DATAPIN4, CLOCKPIN4, DOTSTAR_BGR);
+Adafruit_DotStar strip5 = Adafruit_DotStar(NUMPIXELS, DATAPIN5, CLOCKPIN5, DOTSTAR_BGR);
+Adafruit_DotStar strip6 = Adafruit_DotStar(NUMPIXELS, DATAPIN6, CLOCKPIN6, DOTSTAR_BGR);
+Adafruit_DotStar strip7 = Adafruit_DotStar(NUMPIXELS, DATAPIN7, CLOCKPIN7, DOTSTAR_BGR);
+Adafruit_DotStar strip8 = Adafruit_DotStar(NUMPIXELS, DATAPIN8, CLOCKPIN8, DOTSTAR_BGR);
 
 void setup() {
   strip1.begin(); 
   strip1.show();
   strip2.begin(); 
   strip2.show();
+  strip3.begin(); 
+  strip3.show();
+  strip4.begin(); 
+  strip4.show();
+  strip5.begin(); 
+  strip5.show();
+  strip6.begin(); 
+  strip6.show();
+  strip7.begin(); 
+  strip7.show();
+  strip8.begin(); 
+  strip8.show();
+
+  pinMode(LED_BUILTIN, OUTPUT);
 
   COM.SETUP;
 
   patterns[0].setStrip(&strip1);
-  patterns[1].setStrip(&strip1);
-  patterns[2].setStrip(&strip1);
-  patterns[3].setStrip(&strip2);
+  patterns[1].setStrip(&strip2);
+  patterns[2].setStrip(&strip3);
+  patterns[3].setStrip(&strip4);
+  patterns[4].setStrip(&strip5);
+  patterns[5].setStrip(&strip6);
+  patterns[6].setStrip(&strip7);
+  patterns[7].setStrip(&strip8);
+
+  patterns[0].setLocation(0, NUMPIXELS);
+  patterns[0].aze(6, false);
+  patterns[1].setLocation(0, NUMPIXELS);
+  patterns[1].aze(2, true);
+  patterns[2].setLocation(0, NUMPIXELS);
+  patterns[2].aze(2, false);
+  patterns[3].setLocation(0, NUMPIXELS);
+  patterns[3].aze(6, false);
+  patterns[4].setLocation(0, NUMPIXELS);
+  patterns[4].aze(6, false);
+  patterns[5].setLocation(0, NUMPIXELS);
+  patterns[5].aze(2, true);
+  patterns[6].setLocation(0, NUMPIXELS);
+  patterns[6].aze(2, false);
+  patterns[7].setLocation(0, NUMPIXELS);
+  patterns[7].aze(6, false);
 }
 
 void loop() {
   for (int i = 0; i < numarrays; i++) {
     patterns[i].setPixels();
-    
   }
   strip1.show();
   strip2.show();
+  strip3.show();
+  strip4.show();
   if (COM.available() > 0) {
     int code = COM_READ;
     switch (code) {
       case 8:
-      patterns[0].setLocation(0, 44);
+      patterns[0].setLocation(0, NUMPIXELS);
       patterns[0].scroll(0xff0000, 0x000000, 44, false);
-      patterns[1].setLocation(44, 82);
+      patterns[1].setLocation(0, NUMPIXELS);
       patterns[1].scroll(0xff0000, 0x000000, 4, false);
-      patterns[2].setLocation(82, NUMPIXELS1);
+      patterns[2].setLocation(0, NUMPIXELS);
       patterns[2].scroll(0xff0000, 0x000000, 4, true);
-      patterns[3].setLocation(0, 44);
+      patterns[3].setLocation(0, NUMPIXELS);
       patterns[3].scroll(0xff0000, 0x000000, 44, false);
       break;
       case 7:
-      patterns[0].setLocation(0, 44);
+      patterns[0].setLocation(0, NUMPIXELS);
       patterns[0].scroll(0xff00ff, 0xff7f00, 4, true);
-      patterns[1].setLocation(44, 82);
+      patterns[1].setLocation(0, NUMPIXELS);
       patterns[1].flash(0xff00ff, 0xff7f00, 100);
-      patterns[2].setLocation(82, NUMPIXELS1);
+      patterns[2].setLocation(0, NUMPIXELS);
       patterns[2].flash(0xff00ff, 0xff7f00, 100);
-      patterns[3].setLocation(0, 44);
+      patterns[3].setLocation(0, NUMPIXELS);
       patterns[3].scroll(0xff00ff, 0xff7f00, 4, true);
       break;
       case 6:
-      patterns[0].setLocation(0, 44);
+      patterns[0].setLocation(0, NUMPIXELS);
       patterns[0].scroll(0xffff00, 0xff0000, 4, false);
-      patterns[1].setLocation(44, 82);
+      patterns[1].setLocation(0, NUMPIXELS);
       patterns[1].flash(0xffff00, 0xff0000, 100);
-      patterns[2].setLocation(82, NUMPIXELS1);
+      patterns[2].setLocation(0, NUMPIXELS);
       patterns[2].flash(0xffff00, 0xff0000, 100);
-      patterns[3].setLocation(0, 44);
+      patterns[3].setLocation(0, NUMPIXELS);
       patterns[3].scroll(0xffff00, 0xff0000, 4, false);
       break;
       case 5:
-      patterns[0].setLocation(0, 44);
+      patterns[0].setLocation(0, NUMPIXELS);
       patterns[0].scroll(0xffff00, 0xff7f00, 4, false);
-      patterns[1].setLocation(44, 82);
+      patterns[1].setLocation(0, NUMPIXELS);
       patterns[1].flash(0xffff00, 0xff7f00, 100);
-      patterns[2].setLocation(82, NUMPIXELS1);
+      patterns[2].setLocation(0, NUMPIXELS);
       patterns[2].flash(0xffff00, 0xff7f00, 100);
-      patterns[3].setLocation(0, 44);
+      patterns[3].setLocation(0, NUMPIXELS);
       patterns[3].scroll(0xffff00, 0xff7f00, 4, false);
       break;
       case 4:
-      patterns[0].setLocation(0, 44);
+      patterns[0].setLocation(0, NUMPIXELS);
       patterns[0].scroll(0xffff00, 0x00ff00, 4, false);
-      patterns[1].setLocation(44, 82);
+      patterns[1].setLocation(0, NUMPIXELS);
       patterns[1].flash(0xffff00, 0x00ff00, 100);
-      patterns[2].setLocation(82, NUMPIXELS1);
+      patterns[2].setLocation(0, NUMPIXELS);
       patterns[2].flash(0xffff00, 0x00ff00, 100);
-      patterns[3].setLocation(0, 44);
+      patterns[3].setLocation(0, NUMPIXELS);
       patterns[3].scroll(0xffff00, 0x00ff00, 4, false);
       break;
       case 3:
-      patterns[0].setLocation(0, 44);
+      patterns[0].setLocation(0, NUMPIXELS);
       patterns[0].scroll(0xffff00, 0x000000, 4, false);
-      patterns[1].setLocation(44, 82);
+      patterns[1].setLocation(0, NUMPIXELS);
       patterns[1].flash(0xffff00, 0x000000, 100);
-      patterns[2].setLocation(82, NUMPIXELS1);
+      patterns[2].setLocation(0, NUMPIXELS);
       patterns[2].flash(0xffff00, 0x000000, 100);
-      patterns[3].setLocation(0, 44);
+      patterns[3].setLocation(0, NUMPIXELS);
       patterns[3].scroll(0xffff00, 0x000000, 4, false);
       break;
       case 2:
-      patterns[0].setLocation(0, 44);
+      patterns[0].setLocation(0, NUMPIXELS);
       patterns[0].scroll(0x0000ff, 0x000000, 44, false);
-      patterns[1].setLocation(44, 82);
+      patterns[1].setLocation(0, NUMPIXELS);
       patterns[1].scroll(0x0000ff, 0x000000, 4, false);
-      patterns[2].setLocation(82, NUMPIXELS1);
+      patterns[2].setLocation(0, NUMPIXELS);
       patterns[2].scroll(0x0000ff, 0x000000, 4, true);
-      patterns[3].setLocation(0, 44);
+      patterns[3].setLocation(0, NUMPIXELS);
       patterns[3].scroll(0x0000ff, 0x000000, 44, false);
       break;
       case 1:
-      patterns[0].setLocation(0, 44);
+      patterns[0].setLocation(0, NUMPIXELS);
       patterns[0].aze(6, false);
-      patterns[1].setLocation(44, 82);
+      patterns[1].setLocation(0, NUMPIXELS);
       patterns[1].aze(2, true);
-      patterns[2].setLocation(82, NUMPIXELS1);
+      patterns[2].setLocation(0, NUMPIXELS);
       patterns[2].aze(2, false);
-      patterns[3].setLocation(0, 44);
+      patterns[3].setLocation(0, NUMPIXELS);
       patterns[3].aze(6, false);
       break;
       default:
-      patterns[0].setLocation(0, NUMPIXELS1);
-      patterns[0].goBlack();
-      patterns[1].setLocation(0, 0);
-      patterns[1].goBlack();
-      patterns[2].setLocation(0, 0);
-      patterns[2].goBlack();
-      patterns[3].setLocation(0, NUMPIXELS2);
-      patterns[3].goBlack();
+      patterns[0].setLocation(0, NUMPIXELS);
+      patterns[0].aze(6, false);
+      patterns[1].setLocation(0, NUMPIXELS);
+      patterns[1].aze(2, true);
+      patterns[2].setLocation(0, NUMPIXELS);
+      patterns[2].aze(2, false);
+      patterns[3].setLocation(0, NUMPIXELS);
+      patterns[3].aze(6, false);
+        patterns[4].setLocation(0, NUMPIXELS);
+  patterns[4].aze(6, false);
+  patterns[5].setLocation(0, NUMPIXELS);
+  patterns[5].aze(2, true);
+  patterns[6].setLocation(0, NUMPIXELS);
+  patterns[6].aze(2, false);
+  patterns[7].setLocation(0, NUMPIXELS);
+  patterns[7].aze(6, false);
+//      patterns[0].setLocation(0, NUMPIXELS);
+//      patterns[0].goBlack();
+//      patterns[1].setLocation(0, NUMPIXELS);
+//      patterns[1].goBlack();
+//      patterns[2].setLocation(0, NUMPIXELS);
+//      patterns[2].goBlack();
+//      patterns[3].setLocation(0, NUMPIXELS);
+//      patterns[3].goBlack();
       break;
     }
+    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
   }
 }
